c59cd96f02b6d30576541a358eafa4a5fc139a0e
wenbinai <208176146@qq.com>
2020-09-22 20:12:15 +0800

    init project

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index">
+    <h3>我是首页</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <h3>我是登陆页面</h3>
+    <input v-model="user.username" placeholder="请输入用户名"/>
+    <input v-model="user.password" placeholder="请输入密码" type="password"/>
+    <button @click="login">登陆</button>
+  </div>
+</template>
+
+<script>
+import axios from '@/plugins/axios/MyAxios'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      console.log(axios.defaults.baseURL)
+      const resp = await axios.post('login', this.user)
+      console.log(resp)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
